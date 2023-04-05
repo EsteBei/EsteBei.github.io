a984afbd10ec50b0f790e3c371028fd00478f5dc
Esteban <esteban.beillonnet@gmail.com>
2023-04-05 15:20:12 +0200

    o

--- script/script.js
+++ script/script.js
@@ -1,15 +1,4 @@
-// get a reference to the image element
-var image = document.querySelector('.projectmedia');
-
-// add a click event listener to the image element
-image.addEventListener('click', function() {
-  // get a reference to the detail page element
-  var detailPage = document.querySelector('.detailpage');
-
-  // toggle the display style of the detail page element
-  if (detailPage.style.display === 'none') {
-    detailPage.style.display = 'block';
-  } else {
-    detailPage.style.display = 'none';
+function showdetails() {
+    const list = document.getElementsByClassName(".detailpage").classlist;
+    list.add(".show")
   }
-});
