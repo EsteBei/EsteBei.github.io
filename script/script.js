5223943f49e6dcf3006178b1491e6463369ecfd2
Esteban <esteban.beillonnet@gmail.com>
2023-04-05 17:25:28 +0200

    ....

--- script/script.js
+++ script/script.js
@@ -1,7 +1,11 @@
 function toggleDetails() {
     var details = document.querySelector('.detailpage');
+    console.log(details.style.display);
     if (details.style.display === 'none') {
       details.style.display = 'block';
+      var mybody = document.querySelector('.fakebody');
+      mybody.classList.toggle("blur");
+      mybody.classList.toggle("noscroll");
     } else {
       details.style.display = 'none';
     }
