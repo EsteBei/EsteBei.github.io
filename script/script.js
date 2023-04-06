d73d8e6649a3946fab279f45c515b5c6f80ca2b2
Esteban <esteban.beillonnet@gmail.com>
2023-04-06 19:46:44 +0200

    Most projects

--- script/script.js
+++ script/script.js
@@ -13,3 +13,48 @@ function toggleDetails() {
       document.body.style.overflow = 'auto';
     }
   }
+  function toggleElement() {
+    console.log("test");
+    var element = document.querySelector('.element');
+    if (element.style.display === 'none') {
+      element.style.display = 'block';
+      var mybody = document.querySelector('.fakebody');
+      mybody.classList.add("blur");
+      document.body.style.overflow = 'hidden';
+    } else {
+      element.style.display = 'none';
+      var mybody = document.querySelector('.fakebody');
+      mybody.classList.remove("blur");
+      document.body.style.overflow = 'auto';
+    }
+  }
+  function toggleDetails2() {
+    console.log("test");
+      var details2 = document.querySelector('.detailpage2');
+      if (details2.style.display === 'none') {
+        details2.style.display = 'block';
+        var mybody = document.querySelector('.fakebody');
+        mybody.classList.add("blur");
+        document.body.style.overflow = 'hidden';
+      } else {
+        details2.style.display = 'none';
+        var mybody = document.querySelector('.fakebody');
+        mybody.classList.remove("blur");
+        document.body.style.overflow = 'auto';
+      }
+  }
+  function toggleDetails3() {
+    console.log("test");
+      var details3 = document.querySelector('.detailpage3');
+      if (details3.style.display === 'none') {
+        details3.style.display = 'block';
+        var mybody = document.querySelector('.fakebody');
+        mybody.classList.add("blur");
+        document.body.style.overflow = 'hidden';
+      } else {
+        details3.style.display = 'none';
+        var mybody = document.querySelector('.fakebody');
+        mybody.classList.remove("blur");
+        document.body.style.overflow = 'auto';
+      }
+  }
