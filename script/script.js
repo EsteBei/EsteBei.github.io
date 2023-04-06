be91621bf1eada7d1f6a6b7c3e22af5cd2c4ad01
Esteban <esteban.beillonnet@gmail.com>
2023-04-06 17:39:27 +0200

    fiix

--- script/script.js
+++ script/script.js
@@ -14,3 +14,18 @@ function toggleDetails() {
     }
   }
 
+  function toggleDetails() {
+    console.log("test");
+      var details = document.querySelector('.detailpage2');
+      if (details.style.display === 'none') {
+        details.style.display = 'block';
+        var mybody = document.querySelector('.fakebody');
+        mybody.classList.add("blur");
+        document.body.style.overflow = 'hidden';
+      } else {
+        details.style.display = 'none';
+        var mybody = document.querySelector('.fakebody');
+        mybody.classList.remove("blur");
+        document.body.style.overflow = 'auto';
+      }
+    }
